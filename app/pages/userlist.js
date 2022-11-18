b8fd3fc459c935e0d888d33d607dabc441fd523c
Serval <1289203637@qq.com>
2022-11-18 15:37:10 +0800

    th -> td

--- app/pages/userlist.js
+++ app/pages/userlist.js
@@ -6,29 +6,29 @@ export async function render() {
 
     var r = $("<table></table>");
     r.addClass("am-table");
-    r.append("<thead><tr><th>序号</th><th>用户名</th><th>通过数</th><th>提交数</th></tr></thead>");
+    r.append("<thead><tr><th>#</th><th>用户名</th><th>通过数</th><th>提交数</th></tr></thead>");
     const sheet = await $.app.get("/user/");
     var body = $("<tbody></tbody>");
     for (var i = 0; i < sheet.data.length; i ++) {
         var item = await $.app.get("/user/" + sheet.data[i].uid + "/statistics/");
         var tr = $("<tr></tr>");
-        var th = $("<th></th>")
-        th.text(i + 1);
-        tr.append(th);
-        var th = $("<th></th>");
-        th.append($.app.t.tag.a(null, "#/u/" + sheet.data[i].uid, sheet.data[i].username));
-        tr.append(th);
-        th = $("<th></th>")
-        th.text(item.data.accepted.length);
-        tr.append(th);
-        th = $("<th></th>")
+        var td = $("<td></td>");
+        td.text(i + 1);
+        tr.append(td);
+        var td = $("<td></td>");
+        td.append($.app.t.tag.a(null, "#/u/" + sheet.data[i].uid, sheet.data[i].username));
+        tr.append(td);
+        td = $("<td></td>")
+        td.text(item.data.accepted.length);
+        tr.append(td);
+        td = $("<td></td>")
         var rec = item.data.submissions;
         var sum = 0;
         for (let key in rec) {
             sum = sum + rec[key];
         }
-        th.text(sum);
-        tr.append(th);
+        td.text(sum);
+        tr.append(td);
         body.append(tr);
     }
     r.append(body);
