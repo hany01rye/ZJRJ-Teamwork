651e9c3e0234e7826f07c127cfea4e42f2e5872f
hany01 <hany01@foxmail.com>
2022-11-19 20:48:55 +0800

    login and register

--- app/pages/login.js
+++ app/pages/login.js
@@ -1,10 +1,43 @@
+function myform(text, id, type) {
+	var r = $.app.t.tag.div("am-input-group am-center space");
+	var s = $("<input></input>");
+	s.attr("id", id);
+	s.attr("placeholder", text);
+	s.addClass("am-form-field");
+	if (type == 0) s.attr("type", "username");
+	else s.attr("type", "password");
+	r.append(s);
+	return r;
+}
+
+function mybutton(text, id) {
+	var r = $.app.t.tag.div("space");
+	var s = $("<button type=\"submit\" id=\" + id + \" class=\"am-btn am-btn-primary am-center am-radius\">" + text + "</button>");
+	s.click(async () => {
+		var username = document.getElementById("username").value;
+		var password = document.getElementById("password").value;
+		var res = await $.app.post("/user/login/", {username: username, password: password});
+		if (res.status == 401) {
+			alert("用户名或密码错误！");
+		} else {
+            window.location.href = "#/";
+		}
+	});
+	r.append(s);
+	return r;
+}
+
 export async function render() {
 	return [
 		$.app.t.header(),
 		$.app.t.container([
 			$.app.t.box(6, [
 				$.app.t.tag.div("am-text-center", [
-					$("<h2>欢迎回到 " + $.app.APP_NAME + "</h2>"),
+					$("<h1>欢迎回到 " + $.app.APP_NAME + "</h1>"),
+					myform("用户名", "username", 0),
+					myform("密码", "password", 1),
+					mybutton("登录", "login"),
+					$.app.t.tag.div("am-u-md-12 space", "没有账号？<a href=\"#/register/\">注册</a>"),
 				]),
 			]),
 		]),
---
+++ app/pages/register.js
@@ -0,0 +1,58 @@
+function myform(text, id, type) {
+	var r = $.app.t.tag.div("am-input-group am-center space");
+	var s = $("<input></input>");
+	s.attr("id", id);
+	s.attr("placeholder", text);
+	s.addClass("am-form-field");
+	if (type == 0) s.attr("type", "username");
+	else s.attr("type", "password");
+	r.append(s);
+	return r;
+}
+
+function mybutton(text, id) {
+	var r = $.app.t.tag.div("space");
+	var s = $("<button type=\"submit\" id=\" + id + \" class=\"am-btn am-btn-primary am-center am-radius\">" + text + "</button>");
+	s.click(async () => {
+		var username = document.getElementById("username").value;
+		var email = document.getElementById("email").value;
+		var password = document.getElementById("password").value;
+		var repeat = document.getElementById("repeat").value;
+        if (password != repeat) {
+            alert("前后密码不一致！");
+            return;
+        }
+		var res = await $.app.post("/user/", {username: username, email: email, password: password, repeat: repeat});
+        console.log(res);
+        if (res != 201) {
+            for (let key in res.data) {
+                alert("Error with " + key + ": " + res.data[key][0]);
+                break;
+            }
+        } else {
+            alert("注册成功");
+            window.location.href = "#/login/";
+        }
+	});
+	r.append(s);
+	return r;
+}
+
+export async function render() {
+	return [
+		$.app.t.header(),
+		$.app.t.container([
+			$.app.t.box(6, [
+				$.app.t.tag.div("am-text-center", [
+					$("<h1>欢迎加入 " + $.app.APP_NAME + "</h1>"),
+					myform("用户名", "username", 0),
+					myform("邮箱地址", "email", 0),
+					myform("密码", "password", 1),
+					myform("重复密码", "repeat", 1),
+					mybutton("注册", "register"),
+					$.app.t.tag.div("am-u-md-12 space", "已有账号？<a href=\"#/login/\">登录</a>"),
+				]),
+			]),
+		]),
+	];
+}
